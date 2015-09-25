1464f9fd00069481cda275af78679aa9dcd45700
Alissa Sobo <alissasobo@gmail.com>
2015-09-24 17:59:00 -0700

    :art:

--- server.js
+++ server.js
@@ -2,30 +2,30 @@ var http = require('http');
 var fs = require('fs');
 var queryString = require('querystring');
 var mustache = require('mustache');
+
 var count = 0;
 var people = [];
 
 var server = http.createServer(function(req, res) {
-  if(req.method == 'POST') {
-    console.log("the form was submitted");
+  if (req.method == 'POST') {
     var body = '';
+
     req.on('data', function(data) {
-    body += data;
+      body += data;
     });
+
     req.on('end', function() {
       var formData = queryString.parse(body);
       people.push(formData);
-      console.log(people);
       res.writeHead(301, {'location' : "/"});
       res.end();
     });
-
   } else {
     fs.readFile("index.html", "utf8", function(err, data) {
       res.writeHead(200, {'Content-Type': 'text/html'});
       res.end(mustache.render(data, {
-        hit_count : count,
-        people : people
+        hit_count: count,
+        people: people
       }));
       count = count + 1;
     });
@@ -34,4 +34,4 @@ var server = http.createServer(function(req, res) {
 
 server.listen(8080);
 
-console.log("server running on 8080")
+console.log("server running on 8080");
