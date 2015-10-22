695927223760ab1bb7cc919eb9c2fe9a4b1636c5
Alissa Sobo <alissasobo@gmail.com>
2015-10-21 21:29:26 -0700

    add delete button and setup database

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var http = require('http');
 var fs = require('fs');
 var queryString = require('querystring');
 var mustache = require('mustache');
+var pg = require('pg');
+var conString = "postgres://alissa:@localhost/practice_site";
 
 var count = 0;
 var people = [];
@@ -16,17 +18,51 @@ var server = http.createServer(function(req, res) {
 
     req.on('end', function() {
       var formData = queryString.parse(body);
-      people.push(formData);
+      pg.connect(conString, function(err, client, done) {
+        if(err) {
+          return console.error('error fetching client from pool', err);
+        }
+        client.query('INSERT INTO people (name, sex) VALUES ($1, $2)', [formData.first_name, formData.sex], function(err, result) {
+          done();
+          if(err) {
+            return console.error('error running query', err);
+          }
+          console.log("success");
+        });
+      });
       res.writeHead(301, {'location' : "/"});
       res.end();
     });
+
+  } else if (req.method == 'DELETE') {
+      var name = req.url.slice(1, req.url.length);
+      for (var i = 0; i < people.length; i++) {
+        if (people[i].first_name === name) {
+          people.splice(i, 1);
+        }
+      }
+      res.writeHead(200);
+      res.end();
   } else {
     fs.readFile("index.html", "utf8", function(err, data) {
       res.writeHead(200, {'Content-Type': 'text/html'});
-      res.end(mustache.render(data, {
-        hit_count: count,
-        people: people
-      }));
+      pg.connect(conString, function(err, client, done) {
+        if(err) {
+          return console.error('error fetching client from pool', err);
+        }
+        client.query('select * from people', function(err, result) {
+          done();
+          if (err) {
+            return console.error('error running query', err);
+          }
+          var queryResult = result;
+          res.end(mustache.render(data, {
+            hit_count: count,
+            people: result.rows
+          }));
+        });
+      });
+
       count = count + 1;
     });
   }
